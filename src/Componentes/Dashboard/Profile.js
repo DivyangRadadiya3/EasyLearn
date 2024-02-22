ba9663817f93e64d1795f8149611f575754b2685
Divyang_Radadiya@3 <115993068+DivyangRadadiya3@users.noreply.github.com>
2024-02-22 18:57:04 +0530

    Add files via upload

--- src/Componentes/Dashboard/Profile.js
+++ src/Componentes/Dashboard/Profile.js
@@ -1,12 +1,8 @@
 import React from "react";
-// import { UserAuth } from "../../Context/AuthContext";
 import Profilecard from "./Profilecard";
-// import Login from "../Login";
 
 const Profile = () => {
-  // const { user } = UserAuth();
 
-  // return <>{user ? <Profilecard /> : <Login />}</>;
   return (
     <>
       <Profilecard />
